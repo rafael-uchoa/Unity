40499c4bf82ffaf87a2988a847663759d9f49683
Rafael Uchoa <rafaeluchoa.1996@gmail.com>
2020-04-02 23:19:18 -0300

    Third and fourth class

---
+++ 03-26/CameraRaycast.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NewBehaviourScript : MonoBehaviour
+{
+	void Update()
+	{
+		RaycastHit collisionPoint;
+
+		if (Physics.Raycast(transform.position, transform.forward, out collisionPoint, 20))
+		{
+			if (collisionPoint.transform.gameObject.name == "Cube")
+			{
+				collisionPoint.transform.gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+			}
+			if (collisionPoint.transform.gameObject.name == "Cube2")
+			{
+				collisionPoint.transform.gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+			}
+			if (collisionPoint.transform.gameObject.name == "Cube3")
+			{
+				collisionPoint.transform.gameObject.GetComponent<MeshRenderer>().enabled = false;
+			}
+			Debug.DrawLine(transform.position, collisionPoint.point, Color.green);
+		}
+	}
+}
---
+++ 04-02/playerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+	private Animator animator;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown("a"))
+			animator.Play("attack", -1, 0f);
+
+		if (Input.GetKeyDown("b"))
+			animator.Play("idle", -1, 0f);
+
+		if (Input.GetKeyDown("c"))
+			animator.Play("defense", -1, 0f);
+	}
+}
